538d62ae540789087ec51f0ca52346d9449b6248
Aditia Darma Nst <aldita.forum@gmail.com>
2022-07-11 04:15:51 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'dbs_ada_indonesia',
+    'username' => 'root',
+    'password' => '3ncoding4sc11A@',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'rada_',
+    'strict' => false,
+    'engine' => NULL,
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://forum-rumahada.justhasnah.me',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
